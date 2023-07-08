2cdd8cfb18715bc0f371fa4407f0727ce5008226
Leniton <lenitoncarneiro@gmail.com>
2023-07-08 20:06:10 -0300

    animações do jogador

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,14 +8,17 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] HealthManager healthManager;
     [SerializeField] HealthSystem barraDePoder;
     [SerializeField] Plataform_Script plataform_Script;
+    [SerializeField] Plataform2d_Input plataformInput;
     [SerializeField] Animator animator;
     int damageHash = Animator.StringToHash("Damaged");
+    int attackHash = Animator.StringToHash("Attack");
+    int glideHash = Animator.StringToHash("Gliding");
 
     private void Awake()
     {
         golpe.attacker = gameObject;
         tiro.attacker = gameObject;
-
+        barraDePoder.reference.FullHeal();
     }
 
     private void Update()
@@ -23,13 +26,16 @@ public class PlayerScript : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             golpe.StartAttack();
+            animator.Play(attackHash);
         }
         if (Input.GetKeyDown(KeyCode.E) && barraDePoder.reference.Value > 0)
         {
             barraDePoder.reference.Damage(1);
             tiro.StartAttack();
+            animator.Play(attackHash);
         }
 
         animator.SetBool(damageHash, healthManager.invincible);
+        animator.SetBool(glideHash, plataformInput.isGliding);
     }
 }
