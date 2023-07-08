32950bf2666e61d6bb07aaea193840a3f71c14e3
Leniton <lenitoncarneiro@gmail.com>
2023-07-08 20:14:29 -0300

    HUD melhor

--- Assets/Scripts/UI/HUD_Script.cs
+++ Assets/Scripts/UI/HUD_Script.cs
@@ -2,27 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DefaultExecutionOrder(-1)]
 public class HUD_Script : MonoBehaviour
 {
     [SerializeField] RectTransform hudHealth;
     [SerializeField] HealthSystem health;
+    Vector2 hpBaseSize;
 
     [SerializeField] RectTransform hudPoder;
     [SerializeField] HealthSystem poder;
+    Vector2 poderBaseSize;
 
     private void Awake()
     {
+        hpBaseSize = hudHealth.sizeDelta;
+        poderBaseSize = hudPoder.sizeDelta;
+
         health.reference.onChange += UpdateHealth;
         poder.reference.onChange += UpdatePoder;
     }
 
     void UpdateHealth()
     {
-        hudHealth.sizeDelta = new Vector2(100 * health.reference.Value, 100);
+        hudHealth.sizeDelta = new Vector2(hpBaseSize.x * health.reference.Value, hpBaseSize.y);
     }
 
     void UpdatePoder()
     {
-        hudPoder.sizeDelta = new Vector2(150 * poder.reference.Value, 150);
+        hudPoder.sizeDelta = new Vector2(poderBaseSize.x * poder.reference.Value, poderBaseSize.y);
     }
 }
