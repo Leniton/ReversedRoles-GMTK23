a1dda58d079a77ee8eae6d9ee6ff6dba00b673af
Artur Miranda Queiroz <arturmiranda78@hotmail.com>
2023-07-09 10:06:45 -0300

    Opções de gráfico
    Retirada as opções de gráfico

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -14,57 +14,9 @@ public class MainMenu : MonoBehaviour
     [Header("Confirmation")]
     [SerializeField] private GameObject comfirmationPrompt = null;   
 
-    [Header("Graphics Settings")]
-    [SerializeField] private Slider brightnessSlider = null;
-    [SerializeField] private TMP_Text brightnessTextValue = null;
-    [SerializeField] private float defaultBrightness = 1;
-
-    [Header("Resolution Dropdowns")]
-    public TMP_Dropdown resolutionDropdown;
-    private Resolution[] resolutions;
-
-    private void Start()
-    {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-
-        for (int i = 0; i < resolutions.Length; i++) 
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
-    public void SetResolution(int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
-
-    private bool _isFullScreen;
-    private float _brightnessLevel;
-
     public void StartGame()
     {
         SceneControler.sceneControler.LoadScene(1);
-    }
-
-    public void ExitButton()
-    {
-        Application.Quit();
     }
 
     public void SetVolume(float volume)
@@ -76,27 +28,6 @@ public class MainMenu : MonoBehaviour
     public void VolumeApply()
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-        StartCoroutine(Confirmationbox());
-    }
-
-    public void SetBrightness(float brightness)
-    {
-        _brightnessLevel = brightness;
-        brightnessTextValue.text = brightness.ToString("0.0");
-    }
-
-    public void SetFullScreen(bool isFullscreen)
-    {
-        _isFullScreen = isFullscreen;
-    }
-
-    public void GraphicsApply()
-    {
-        PlayerPrefs.SetFloat("masterBrightness", _brightnessLevel);
-
-        PlayerPrefs.SetInt("masterFullScreen", (_isFullScreen ? 1 : 0));
-        Screen.fullScreen = _isFullScreen;
-
         StartCoroutine(Confirmationbox());
     }
 
